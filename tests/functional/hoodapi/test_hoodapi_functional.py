247ad22ad94a1d2a24928994a911cc6db7b393b5
etnberz <maxime.caitucoli@live.fr>
2023-05-30 17:58:11 +0200

    test get open order

--- tests/functional/hoodapi/test_hoodapi_functional.py
+++ tests/functional/hoodapi/test_hoodapi_functional.py
@@ -1,3 +1,5 @@
+from pathlib import Path
+
 import duckdb
 import numpy as np
 import pandas as pd
@@ -7,7 +9,9 @@ from robothoodash.hoodapi.hoodapi import HoodApi
 
 np.random.seed(42)
 
-TEST_DATA = pd.DataFrame(
+TEST_DATA_PATH = Path(__file__).parent.parent / "data"
+
+TEST_DATA_TRACKER = pd.DataFrame(
     {
         "timestamp": pd.date_range(start="2023-05-25 00:00", freq="H", periods=24),
         "btc_balance": np.random.normal(loc=0.07, scale=0.004, size=24),
@@ -15,19 +19,34 @@ TEST_DATA = pd.DataFrame(
     }
 )
 
+TEST_DATA_ORDERS = pd.read_parquet(TEST_DATA_PATH / "open_order_test_data.parquet")
+
 
 @pytest.mark.parametrize(
-    "base_currency, tracker", [("btc", TEST_DATA), ("usdt", TEST_DATA)], ids=["BTC", "USDT"]
+    "base_currency, tracker",
+    [("btc", TEST_DATA_TRACKER), ("usdt", TEST_DATA_TRACKER)],
+    ids=["BTC", "USDT"],
 )
 def test_get_balance_data_base_currency(
-    mocker, tracker, base_currency
+        mocker, tracker, base_currency
 ):  # pylint:disable=unused-argument
     mocker.patch("robothoodash.hoodapi.hoodapi.duckdb.connect", return_value=duckdb.connect())
     hood_api = HoodApi()
     result = hood_api.get_balance_data(base_currency=base_currency)
     expected_result = (
-        TEST_DATA[["timestamp", "btc_balance"]]
+        TEST_DATA_TRACKER[["timestamp", "btc_balance"]]
         if base_currency == "btc"
-        else TEST_DATA[["timestamp", "usdt_balance"]]
+        else TEST_DATA_TRACKER[["timestamp", "usdt_balance"]]
     )
+    pd.testing.assert_frame_equal(left=result, right=expected_result)
+
+
+def test_get_open_orders(mocker):  # pylint:disable=unused-argument
+    trading_signal = TEST_DATA_ORDERS
+    mocker.patch("robothoodash.hoodapi.hoodapi.duckdb.connect", return_value=duckdb.connect())
+    hood_api = HoodApi()
+    result = hood_api.get_open_orders()
+    expected_result = trading_signal[trading_signal["open"] == 1][
+        ["pair", "quantity", "strategy", "target_1", "target_2", "status"]
+    ].reset_index(drop=True)
     pd.testing.assert_frame_equal(left=result, right=expected_result)
